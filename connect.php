d8d6b52f32947bf1d2d39d6c3f10e7b3c8043b20
luongnvgch17448 <luongnvgch17448@fpt.edu.vn>
2019-09-01 12:54:24 +0700

    Connect Changed

--- connect.php
+++ connect.php
@@ -1,5 +1,7 @@
 <?php
-$conn = pg_connect("host=? port=? dbname=? 
-user=? password=?") 
+$conn = pg_connect("host=ec2-54-83-9-36.compute-1.amazonaws.com
+port=5432 dbname=d41kgcl1fqopg9 
+user=lvdapdhxptgeib
+password=5e165075505bb5928dbf1d393c0e00258414b53f6488c1e9baf84a1de10bc8a2") 
 or die("Connect Unsuccessful");
 ?>
